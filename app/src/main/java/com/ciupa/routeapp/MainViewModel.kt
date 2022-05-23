056a6a21137c544d6806575ee1df60f5d4c29e00
Łukasz Ciupa <lukciupa@gmail.com>
2022-05-23 23:13:37 +0200

    Fetching cards in viewModelScope

--- app/src/main/java/com/ciupa/routeapp/MainViewModel.kt
+++ app/src/main/java/com/ciupa/routeapp/MainViewModel.kt
@@ -4,25 +4,30 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ciupa.routeapp.domain.RouteInteractor
 import com.ciupa.routeapp.model.TransportCardType
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     private val _cardList = MutableLiveData<List<TransportCardType.TransportCard>>(emptyList())
     val cardList: LiveData<List<TransportCardType.TransportCard>> = _cardList
-    private val cards : List<TransportCardType.TransportCard>
     // Injected in normal project
     private val routeInteractor = RouteInteractor()
 
     init {
-        cards = routeInteractor.fetchCards()
-        _cardList.value = cards
+        viewModelScope.launch {
+            val cards = routeInteractor.fetchCards()
+            _cardList.value = cards
+        }
     }
 
     fun onSortClick() {
-        val sortedCards = routeInteractor.findRoute(cards)
-        Log.d("Result", "Sorted elements: $sortedCards")
-        _cardList.value = sortedCards
+        cardList.value?.let {
+            val sortedCards = routeInteractor.findRoute(it)
+            Log.d("Result", "Sorted elements: $sortedCards")
+            _cardList.value = sortedCards
+        }
     }
 }
